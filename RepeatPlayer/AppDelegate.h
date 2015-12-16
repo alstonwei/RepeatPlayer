a9ed8a2a4a0f0c2add9cfa68380d6f08fa7c9fc3
weishouqiang <weishouqaing@epailive.com>
2015-12-16 19:08:10 +0800

    Initial Commit

---
+++ RepeatPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RepeatPlayer
+//
+//  Created by Shouqiang Wei on 15/12/16.
+//  Copyright © 2015年 魏守强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RepeatPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RepeatPlayer
+//
+//  Created by Shouqiang Wei on 15/12/16.
+//  Copyright © 2015年 魏守强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
